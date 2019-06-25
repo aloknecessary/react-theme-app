3431cf0e6fb9c2e9a4cb38f70998da9f48deba5a
Alok Ranjan <aloks@byteridge.com>
2019-06-25 15:03:05 +0530

    added initial build of app; generated by CLI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Initial commit of React Theme App.</h2>
+      <p>An awesome app is on its way!</p>
+    </div>
+  );
+}
+
+export default App;
